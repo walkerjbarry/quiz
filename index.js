2f27a4479964ed16bc7aa641b4680ddea6cb07e9
barry_walker <barry.walker@esc13.txed.net>
2022-08-04 15:43:34 -0500

    Committing updated html, js, and css files.

---
+++ index.js
@@ -0,0 +1,22 @@
+/*Project 6: Build a three - question quiz.
+
+    Requirements:
+-Create a Github repository for this project. 
+- Create an HTML form with a three - question quiz
+    - Each question should have four multiple - choice answers to choose from.
+- When the user submits the quiz, add a message underneath each question letting them know if they got it right / wrong(give them the correct answer).*/
+
+
+function check() {
+    let i = 0;
+    let q1 = document.quiz.question1.value;
+    let q2 = document.quiz.question2.value;
+    let q3 = document.quiz.question3.value;
+    let result = document.getElementById('result');
+    let quiz = document.getElementById('quiz');
+    if (q1 == "Austin") { i++ };
+    if (q2 == "El Paso") { i++ };
+    if (q3 == "I-35") { i++ };
+    quiz.style.display = "none";
+    result.textContent = `${i}`;
+};
